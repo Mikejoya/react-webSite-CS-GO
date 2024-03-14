3c63d14356b356df22d68d8ce22376eb75a088dd
mikejoya <michaeloziljoya@gmail.com>
2024-03-13 23:17:15 -0500

    se agrega una config al archivo de cofiguracion de vite y se elimina carpeta dist para usar en github el page actions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "react-webSite-CS-GO",
   plugins: [react()],
+  base: "https://Mikejoya.github.io/react-webSite-CS-GO",
 });
